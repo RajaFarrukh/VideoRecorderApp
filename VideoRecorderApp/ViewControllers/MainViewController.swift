5833528b9467ba0539667c462c2df6ebc865b0e5
AskariBank <RajaFarrukh@users.noreply.github.com>
2022-10-13 16:20:05 +0500

    adding camera layer

--- VideoRecorderApp/ViewControllers/MainViewController.swift
+++ VideoRecorderApp/ViewControllers/MainViewController.swift
@@ -8,52 +8,44 @@
 import UIKit
 import AVFoundation
 import AssetsLibrary
+import MobileCoreServices
+import AVKit
+import Photos
+
 
 class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate, UIGestureRecognizerDelegate {
     
-    @IBOutlet var recordOutlet: UIButton!
-    @IBOutlet var recordLabel: UILabel!
+    let videoPickerController = UIImagePickerController()
+    var isVideoRecordingStart:Bool = false
+    var capturedVideoURL:URL? = nil
+    var timer = Timer()
+    var timerForClip = Timer()
+    var counter = 0
+    var recordingSlot = 4
+    var videoTimeInSec = 4
     
-    @IBOutlet var cameraView: UIView!
-    var tempImage: UIImageView?
-    
-    private var session: AVCaptureSession = AVCaptureSession()
-    private var deviceInput: AVCaptureDeviceInput?
-    private var previewLayer: AVCaptureVideoPreviewLayer?
-    private var videoOutput: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
-    private var audioOutput: AVCaptureAudioDataOutput = AVCaptureAudioDataOutput()
-    
-   // private var videoDevice: AVCaptureDevice = AVCaptureDevice.default(for: .video) //AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-    private var audioConnection: AVCaptureConnection?
-    private var videoConnection: AVCaptureConnection?
-    
-    private var assetWriter: AVAssetWriter?
-    private var audioInput: AVAssetWriterInput?
-    private var videoInput: AVAssetWriterInput?
-    
-    private var fileManager: FileManager = FileManager()
-    private var recordingURL: URL?
-    
-    private var isCameraRecording: Bool = false
-    private var isRecordingSessionStarted: Bool = false
-    
-    private var recordingQueue = DispatchQueue(label: "recording.queue")
-    var captureSession: AVCaptureSession?
-    var stillImageOutput: AVCapturePhotoOutput?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var currentCaptureDevice: AVCaptureDevice?
-    
-    var usingFrontCamera = false
-    
-    /* This is the function i want to use to start
-     recording a video */
-    
+    @IBOutlet var startStopButton: UIButton!
+    @IBOutlet var videoPickerControllerView: UIView!
+    @IBOutlet var videoTimerLabel: UILabel!
+    @IBOutlet var clipTimeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        self.setup()
+        // self.setup()
+        self.createAlbum()
+        self.clipTimeLabel.isHidden = true
+        self.clipTimeLabel.text = ""
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.cameraViewAction()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
     
     
@@ -68,167 +60,28 @@ class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
      */
     
     
-    /* This is the function i want to use to start
-     recording a video */
-
-    private func setup() {
-        self.session.sessionPreset = AVCaptureSession.Preset.high
-        
-        self.recordingURL = URL(fileURLWithPath: "\(NSTemporaryDirectory() as String)/file.mov")
-        if self.fileManager.isDeletableFile(atPath: self.recordingURL!.path) {
-            _ = try? self.fileManager.removeItem(atPath: self.recordingURL!.path)
-        }
-        
-        self.assetWriter = try? AVAssetWriter(outputURL: self.recordingURL!,
-                                              fileType: AVFileType.mov)
-        
-        let audioSettings = [
-            AVFormatIDKey : kAudioFormatAppleIMA4,
-            AVNumberOfChannelsKey : 1,
-            AVSampleRateKey : 16000.0
-        ] as [String : Any]
-        
-        let videoSettings = [
-            AVVideoCodecKey : AVVideoCodecH264,
-            AVVideoWidthKey : UIScreen.main.bounds.size.width,
-            AVVideoHeightKey : UIScreen.main.bounds.size.height
-        ] as [String : Any]
-        
-        self.videoInput = AVAssetWriterInput(mediaType: AVMediaType.video,
-                                             outputSettings: videoSettings)
-        self.audioInput = AVAssetWriterInput(mediaType: AVMediaType.audio,
-                                             outputSettings: audioSettings)
-        
-        self.videoInput?.expectsMediaDataInRealTime = true
-        self.audioInput?.expectsMediaDataInRealTime = true
-        
-        if self.assetWriter!.canAdd(self.videoInput!) {
-            self.assetWriter?.add(self.videoInput!)
-        }
-        
-        if self.assetWriter!.canAdd(self.audioInput!) {
-            self.assetWriter?.add(self.audioInput!)
-        }
-        
-        if let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) {
-//           let input = AVCaptureDeviceInput(device: captureDevice)
-//           session.addInput(input)
-            self.deviceInput = try? AVCaptureDeviceInput(device: captureDevice)
-        }
-        
-        if let deviceInputt = self.deviceInput {
-            if self.session.canAddInput(deviceInputt) {
-                self.session.addInput(deviceInputt)
+    func cameraViewAction() {
+        videoPickerController.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == false {
+            DispatchQueue.main.async {
+                //OptiToast.showNegativeMessage(message: OptiConstant().cameranotavailable)
+                //
             }
+            return
         }
+        videoPickerController.allowsEditing = true
+        videoPickerController.sourceType = .camera
+        videoPickerController.mediaTypes = [kUTTypeMovie as String]
+        videoPickerController.videoMaximumDuration = TimeInterval(240.0)
+        videoPickerController.cameraCaptureMode = .video
+        videoPickerController.modalPresentationStyle = .overCurrentContext//.fullScreen
         
-        self.previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
-        self.previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+        addChild(videoPickerController)
+        self.videoPickerControllerView.addSubview(videoPickerController.view)
+        videoPickerController.view.frame = self.videoPickerControllerView.bounds
+        videoPickerController.allowsEditing = false
+        videoPickerController.showsCameraControls = false
         
-        let rootLayer = self.view.layer
-        rootLayer.masksToBounds = true
-        self.previewLayer?.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        
-        let statusBarOrientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
-        let videoOrientation: AVCaptureVideoOrientation = statusBarOrientation?.videoOrientation ?? .landscapeRight
-        //videoPreviewLayer.frame = view.layer.bounds
-        self.previewLayer?.connection?.videoOrientation = videoOrientation
-       // videoPreviewLayer.removeAllAnimations()
-        
-        rootLayer.insertSublayer(self.previewLayer!, at: 0)
-        
-        self.session.startRunning()
-        
-        DispatchQueue.main.async {
-            self.session.beginConfiguration()
-            
-            if self.session.canAddOutput(self.videoOutput) {
-                self.session.addOutput(self.videoOutput)
-            }
-            
-            self.videoConnection = self.videoOutput.connection(with: AVMediaType.video)
-            if self.videoConnection?.isVideoStabilizationSupported == true {
-                self.videoConnection?.preferredVideoStabilizationMode = .auto
-            }
-            self.session.commitConfiguration()
-            
-            if let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio) {
-                //           let input = AVCaptureDeviceInput(device: captureDevice)
-                //           session.addInput(input)
-                //let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
-                
-                if let audioIn = try? AVCaptureDeviceInput(device: audioDevice) {
-                    
-                    if self.session.canAddInput(audioIn) {
-                        self.session.addInput(audioIn)
-                    }
-                }
-                
-                if self.session.canAddOutput(self.audioOutput) {
-                    self.session.addOutput(self.audioOutput)
-                }
-                
-            }
-            
-            
-           
-            self.audioConnection = self.audioOutput.connection(with: AVMediaType.audio)
-            
-        }
-    }
-    
-    private func startRecording() {
-        if self.assetWriter?.startWriting() != true {
-            print("error: \(self.assetWriter?.error.debugDescription ?? "")")
-        }
-        
-        self.videoOutput.setSampleBufferDelegate(self, queue: self.recordingQueue)
-        self.audioOutput.setSampleBufferDelegate(self, queue: self.recordingQueue)
-    }
-    
-    private func stopRecording() {
-        self.videoOutput.setSampleBufferDelegate(nil, queue: nil)
-        self.audioOutput.setSampleBufferDelegate(nil, queue: nil)
-        
-        self.assetWriter?.finishWriting {
-            print("saved")
-        }
-    }
-    
-    @IBAction func recordingButton(_ sender: Any) {
-        if self.isCameraRecording {
-            self.stopRecording()
-        } else {
-            self.startRecording()
-        }
-        self.isCameraRecording = !self.isCameraRecording
-    }
-    
-    
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer
-                       sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        
-        if !self.isRecordingSessionStarted {
-            let presentationTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
-            self.assetWriter?.startSession(atSourceTime: presentationTime)
-            self.isRecordingSessionStarted = true
-        }
-        
-        let description = CMSampleBufferGetFormatDescription(sampleBuffer)!
-        
-        if CMFormatDescriptionGetMediaType(description) == kCMMediaType_Audio {
-            if self.audioInput!.isReadyForMoreMediaData {
-                print("appendSampleBuffer audio");
-                self.audioInput?.append(sampleBuffer)
-            }
-        } else {
-            if self.videoInput!.isReadyForMoreMediaData {
-                print("appendSampleBuffer video");
-                if !self.videoInput!.append(sampleBuffer) {
-                    print("Error writing video buffer");
-                }
-            }
-        }
     }
     
     /*
@@ -244,6 +97,91 @@ class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
         }
     }
     
+    func startStopRecording() {
+        if isVideoRecordingStart {
+            isVideoRecordingStart = false
+            self.videoPickerController.stopVideoCapture()
+            counter = 0
+            timer.invalidate()
+            self.startStopButton.setTitle("Start", for: .normal)
+            self.startStopButton.backgroundColor = AppUtility.hexColor(hex: "2E6A1F")
+        } else {
+            isVideoRecordingStart = true
+            self.videoPickerController.startVideoCapture()
+            self.startStopButton.setTitle("Stop", for: .normal)
+            self.startStopButton.backgroundColor = UIColor.red
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        }
+    }
+    
+    // called every time interval from the timer
+    @objc func timerAction() {
+        counter += 1
+        let timerFormatedTime = self.secondsToHoursMinutesSeconds(counter)
+        self.videoTimerLabel.text = "\(timerFormatedTime.hours):\(timerFormatedTime.minutes):\(timerFormatedTime.seconds)"
+    }
+    
+    func secondsToHoursMinutesSeconds(_ seconds: Int) -> (hours:Int, minutes:Int, seconds:Int) {
+        return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    }
+    
+    // called every time interval from the timer
+    @objc func timerActionForClip() {
+        recordingSlot -= 1
+        if recordingSlot < 0 {
+            self.timerForClip.invalidate()
+            clipTimeLabel.isHidden = true
+            self.startStopRecording()
+            self.clipTimeLabel.text = ""
+            self.startStopRecording()
+            self.startStopRecording()
+        } else {
+            self.clipTimeLabel.isHidden = false
+            self.clipTimeLabel.text = "\(recordingSlot)"
+        }
+    }
+    
+    func export(_ asset: AVAsset, to outputMovieURL: URL, startTime: CMTime, endTime: CMTime, composition: AVVideoComposition) {
+
+        //Create trim range
+      let timeRange = CMTimeRangeFromTimeToTime(start: startTime, end: endTime)
+
+        //delete any old file
+      do {
+        try FileManager.default.removeItem(at: outputMovieURL)
+      } catch {
+        print("Could not remove file \(error.localizedDescription)")
+      }
+
+        //create exporter
+      let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality)
+
+        //configure exporter
+      exporter?.videoComposition = composition
+      exporter?.outputURL = outputMovieURL
+      exporter?.outputFileType = .mov
+      exporter?.timeRange = timeRange
+
+        //export!
+      exporter?.exportAsynchronously(completionHandler: { [weak exporter] in
+        DispatchQueue.main.async {
+          if let error = exporter?.error {
+            print("failed \(error.localizedDescription)")
+          } else {
+            print("Video saved to \(outputMovieURL)")
+          }
+        }
+      })
+    }
+    
+    func secondsFromString (string: String) -> Int {
+        let components: Array = string.components(separatedBy: ":")
+        let hours = Int(components[0] ) ?? 0
+        let minutes = Int(components[1] ) ?? 0
+        let seconds = Int(components[2] ) ?? 0
+        return Int(((hours * 60) * 60) + (minutes * 60) + seconds)
+    }
+    
     // MARK: - IBAction Methods
     
     /*
@@ -254,7 +192,78 @@ class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBuffer
         self.goToSelectDurationViewController()
     }
     
+    /*
+     // Method: onBtnSignup
+     // Description: IBAction for signup button
+     */
+    @IBAction func onBtnStartRecording(_ sender: Any) {
+        self.startStopRecording()
+    }
     
+    @IBAction func onBtnMakes(_ sender: Any) {
+        timerForClip = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerActionForClip), userInfo: nil, repeats: true)
+    }
+    
+}
+
+//MARK : UIImagePicker Delegate
+extension MainViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIViewControllerTransitioningDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL
+        print(videoURL)
+        capturedVideoURL = videoURL
+        
+        if let cVideoURL = self.capturedVideoURL {
+            let currentTime = self.videoTimerLabel.text ?? "0:0:0" // hh:mm:ss
+            
+            let currentTimeInSec = self.secondsFromString(string: currentTime)
+            
+            let startSec = Float64(currentTimeInSec - 4)
+            let endSec = Float64(currentTimeInSec + 4)
+            
+            let smallValue =  0.0401588716
+            let startSecCMTime =      CMTimeMakeWithSeconds(startSec, preferredTimescale: 1)
+            let startTimeInCM =   CMTimeMultiplyByFloat64(startSecCMTime , multiplier: smallValue)
+            print(CMTimeGetSeconds(startTimeInCM))
+            
+            let endSecCMTime =      CMTimeMakeWithSeconds(endSec, preferredTimescale: 1)
+            let endTimeInCM =   CMTimeMultiplyByFloat64(endSecCMTime , multiplier: smallValue)
+            print(CMTimeGetSeconds(endTimeInCM))
+            let asset = AVAsset(url: cVideoURL)
+            
+            let fetchOptions = PHFetchOptions()
+            fetchOptions.predicate = NSPredicate(format: "title = %@", "VIdeoClipss")
+            let collections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+            print("collections")
+            collections.
+            
+          
+            //self.export(asset, to: URL(string: galleryPath), startTime: startSecCMTime, endTime: endTimeInCM, composition: composition)
+           
+            print("Done")
+        }
+        
+        //videoTimeInSec
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
+    
+    func createAlbum() {
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: "VIdeoClipss")   // create an asset collection with the album name
+        }) { success, error in
+            if success {
+              
+            } else {
+                print("error \(error)")
+            }
+        }
+    }
+
 }
 
 extension UIInterfaceOrientation {
@@ -268,3 +277,4 @@ extension UIInterfaceOrientation {
         }
     }
 }
+ 
